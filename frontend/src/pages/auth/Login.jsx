3cc36159157b2c108fad7bda43a29686a8a259d9
krupali-jadav <jadavkrupali99@gmail.com>
2026-05-27 10:00:18 +0530

    add password show and hide logic

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -5,121 +5,140 @@ import API from "../services/api";
 import { AuthContext } from "../context/AuthContext";
 
 import { useNavigate } from "react-router-dom";
+import { EyeIcon, EyeOff } from "lucide-react";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
 
-    const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [role, setRole] = useState("admin");
+  const [showPassword, setShowPassword] = useState(false);
 
-    const { login } = useContext(AuthContext);
+  const [role, setRole] = useState("admin");
 
-    const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const navigate = useNavigate();
 
-        try {
-            let res;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-            // Admin Login
+    try {
+      let res;
 
-            if (role === "admin") {
-                res = await API.post("/admin/login", {
-                    email,
-                    password,
-                });
+      // Admin Login
 
-                login({
-                    user: {
-                        ...res.data.admin,
+      if (role === "admin") {
+        res = await API.post("/admin/login", {
+          email,
+          password,
+        });
 
-                        role: "admin",
-                    },
+        login({
+          user: {
+            ...res.data.admin,
 
-                    token: res.data.token,
-                });
+            role: "admin",
+          },
 
-                navigate("/dashboard");
-            }
+          token: res.data.token,
+        });
 
-            // Student Login
-            else {
-                res = await API.post("/student/login", {
-                    email,
-                    password,
-                });
+        navigate("/dashboard");
+      }
 
-                login({
-                    user: {
-                        ...res.data.student,
+      // Student Login
+      else {
+        res = await API.post("/student/login", {
+          email,
+          password,
+        });
 
-                        role: "student",
-                    },
+        login({
+          user: {
+            ...res.data.student,
 
-                    token: res.data.token,
-                });
+            role: "student",
+          },
 
-                navigate("/student-dashboard");
-            }
-        } catch (error) {
-            alert(error.response?.data?.message || "Login Failed");
-        }
-    };
+          token: res.data.token,
+        });
 
-    return (
-        <div className="flex items-center justify-center h-screen bg-gray-100">
-            <form
-                onSubmit={handleSubmit}
-                className="bg-white p-8 rounded-lg shadow-lg w-96"
-            >
-                <h2 className="text-3xl font-bold mb-6 text-center">Login</h2>
+        navigate("/student-dashboard");
+      }
+    } catch (error) {
+      alert(error.response?.data?.message || "Login Failed");
+    }
+  };
 
-                {/* Role */}
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded-lg shadow-lg w-96"
+      >
+        <h2 className="text-3xl font-bold mb-6 text-center">Login</h2>
 
-                <select
-                    className="w-full border p-2 mb-3 rounded"
-                    value={role}
-                    onChange={(e) => setRole(e.target.value)}
-                >
-                    <option value="admin">Admin</option>
+        {/* Role */}
 
-                    <option value="student">Student</option>
-                </select>
+        <select
+          className="w-full border p-2 mb-3 rounded"
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+        >
+          <option value="admin">Admin</option>
 
-                {/* Email */}
+          <option value="student">Student</option>
+        </select>
 
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    className="w-full border p-2 mb-3 rounded"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
+        {/* Email */}
 
-                {/* Password */}
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          className="w-full border p-2 mb-3 rounded"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    className="w-full border p-2 mb-4 rounded"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+        {/* Password */}
 
-                {/* Button */}
+        <div className="relative">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            value={password}
+            className="w-full border p-2 mb-4 rounded"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                <button className="bg-blue-500 hover:bg-blue-600 text-white w-full py-2 rounded">
-                    Login
-                </button>
-
-                <p className="text-center mt-4 text-sm text-gray-500">
-                    Student accounts are created by admin only.
-                </p>
-            </form>
+          {showPassword ? (
+            <EyeOff
+              className="absolute top-3 right-3 cursor-pointer text-gray-400"
+              size={20}
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <EyeIcon
+              className="absolute top-3 right-3 cursor-pointer text-gray-400"
+              size={20}
+              onClick={() => setShowPassword(true)}
+            />
+          )}
         </div>
-    );
+
+        {/* Button */}
+
+        <button className="bg-blue-500 hover:bg-blue-600 text-white w-full py-2 rounded">
+          Login
+        </button>
+
+        <p className="text-center mt-4 text-sm text-gray-500">
+          Student accounts are created by admin only.
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
