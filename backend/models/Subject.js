00d6b3a495fc2c31f2d172e6dba2445fb6cf00e1
nidhi685 <nidhimakwana8910@gmail.com>
2026-05-28 18:30:14 +0530

    create model for admin side

---
+++ backend/models/Subject.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const subjectSchema = new mongoose.Schema({
+  subjectName: {
+    type: String,
+    required: true,
+  },
+
+  subjectCode: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Subject", subjectSchema);
